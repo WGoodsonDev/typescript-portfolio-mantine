39caa999ebfe76329c38949cf8fb4ce14b197067
WGoodsonDev <warren.goodson1@gmail.com>
2022-01-26 17:28:50 -0800

    Skill chips

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,14 +1,62 @@
 import React from "react";
-import { Container, Grid, Card } from '@mantine/core';
+import { 
+    Container, 
+    Grid, 
+    Card, 
+    Paper, 
+    Group, 
+    Title, 
+    Badge, 
+    Chips, 
+    Chip, 
+    Divider 
+} from '@mantine/core';
 
 export default function Content() {
     return (
         <Container fluid style={{ margin: '20px' }}>
-            <Grid justify={'center'}>
-                <Grid.Col span={6}>
-                    <Card>
+            <Grid 
+                justify={"center"}
+                align={"center"}
+                gutter={"xs"}
+                style={{height: "inherit"}}
+            >
+                <Grid.Col span={12}>
+                    <Paper padding="lg">
+                        <Group position="apart" style={{marginBottom: "1rem"}}>
+                            <Title order={1}>Warren&apos;s Tech Stack</Title>
+                        </Group>
+                        <Card>
+                            <Container fluid>
+                                <Grid gutter={"sm"}>
+                                    <Grid.Col>
+                                        <Badge style={{marginBottom: "12px"}}>
+                                            Skill Category 1
+                                        </Badge>
+                                        <Chips radius="sm" multiple defaultValue={['react']} style={{display: "flex"}}>
+                                            <Chip value="react">React</Chip>
+                                            <Chip value="react">React</Chip>
+                                            <Chip value="react">React</Chip>
+                                            <Chip value="react">React</Chip>
+                                        </Chips>
+                                        <Divider style={{ margin: "10px 0px 10px 0px" }} />
+                                        <Badge style={{marginBottom: "12px"}}>
+                                            Skill Category 2
+                                        </Badge>
+                                        <Chips radius="sm" multiple defaultValue={['react']} style={{display: "flex"}}>
+                                            <Chip value="react">React</Chip>
+                                            <Chip value="react">React</Chip>
+                                            <Chip value="react">React</Chip>
+                                            <Chip value="react">React</Chip>
+                                        </Chips>
+                                    </Grid.Col>
 
-                    </Card>
+                                </Grid>
+                            </Container>
+
+                        </Card>
+                    </Paper>
+                    
                 </Grid.Col>
 
             </Grid>
