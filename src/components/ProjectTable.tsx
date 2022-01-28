c2d92841f5d9623fe2e02c8dceed15de7e57e256
WGoodsonDev <warren.goodson1@gmail.com>
2022-01-27 19:42:02 -0800

    Projects Table filled out

--- src/components/ProjectTable.tsx
+++ src/components/ProjectTable.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { 
-    Card, Table, 
+    Card, 
+    Table, 
+    Text
 } from "@mantine/core";
 
 export default function ProjectTable() {
@@ -18,22 +20,38 @@ export default function ProjectTable() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>Project Name</td>
-                        <td>Description</td>
-                        <td>Skills Used</td>
-                        <td>LINK</td>
+                        <td>SSBM Combo Visualizer</td>
+                        <td>Interactive combo visualizer for Super Smash Bros. Melee powered by Slippi. Upload game replay files to the site to help analyze gameplay in an intuitive, visual way.</td>
+                        <td>Slippi-js | React | D3 | Express | MongoDB </td>
+                        <td>
+                            <Text variant="link" component="a" href="http://ssbm-combo-viewer.tech/">Demo</Text>
+                            <br/>
+                            <Text variant="link" component="a" href="https://github.com/WGoodsonDev/ssbm-combo-visualizer">Source</Text>
+                        </td>
                     </tr>
                     <tr>
-                        <td>Project Name</td>
-                        <td>Description</td>
-                        <td>Skills Used</td>
-                        <td>LINK</td>
+                        <td>SSU Scheduler</td>
+                        <td>Full-stack application for scheduling classes in higher education. Senior Capstone Project at Sonoma State University.</td>
+                        <td>React | D3 | MySQL | Koa</td>
+                        <td>Proprietary</td>
                     </tr>
                     <tr>
-                        <td>Project Name</td>
-                        <td>Description</td>
-                        <td>Skills Used</td>
-                        <td>LINK</td>
+                        <td>Octane</td>
+                        <td>Top-down, card-based, bullet hell shoot-em-up game with challenging boss fights. Awarded Winner: Best Gameplay at Sonoma State Tech Showcase by industry professionals from Visual Concepts, developers of the NBA 2K series.</td>
+                        <td>Unreal Engine 4 | C++ | Blueprint</td>
+                        <td>
+                            <Text variant="link" component="a" href="https://github.com/ogre9k/CS330_Final">Source</Text>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Profile Site</td>
+                        <td>Profile website for showing off my skills.</td>
+                        <td>React | TypeScript | Mantine UI</td>
+                        <td>
+                            <Text variant="link" component="a" href="http://warrengoodson.tech/">Demo</Text>
+                            <br/>
+                            <Text variant="link" component="a" href="https://github.com/WGoodsonDev/typescript-portfolio-mantine">Source</Text>
+                        </td>
                     </tr>
                 </tbody>
             </Table>
