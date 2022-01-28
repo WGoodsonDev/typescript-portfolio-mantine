eedeb9b069210259a0696a22c2abff284deb4397
WGoodsonDev <warren.goodson1@gmail.com>
2022-01-27 19:49:56 -0800

    Links, formatting added to Project Table

--- src/components/ProjectTable.tsx
+++ src/components/ProjectTable.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { 
-    Card, 
-    Table, 
+import {
+    Card,
+    Table,
     Text
 } from "@mantine/core";
 
 export default function ProjectTable() {
     return (
         <Card>
-            <br/>
+            <br />
             <Table highlightOnHover>
                 <thead>
                     <tr>
@@ -20,37 +20,85 @@ export default function ProjectTable() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>SSBM Combo Visualizer</td>
+                        <td>
+                            <Text
+                                component="span"
+                                align="center"
+                                variant="gradient"
+                                gradient={{ from: 'indigo', to: 'cyan', deg: 60 }}
+                                size="xl"
+                                weight={700}
+                                style={{ fontFamily: 'Greycliff CF, sans-serif' }}
+                            >
+                                SSBM Combo Visualizer
+                            </Text>
+                        </td>
                         <td>Interactive combo visualizer for Super Smash Bros. Melee powered by Slippi. Upload game replay files to the site to help analyze gameplay in an intuitive, visual way.</td>
                         <td>Slippi-js | React | D3 | Express | MongoDB </td>
                         <td>
-                            <Text variant="link" component="a" href="http://ssbm-combo-viewer.tech/">Demo</Text>
-                            <br/>
-                            <Text variant="link" component="a" href="https://github.com/WGoodsonDev/ssbm-combo-visualizer">Source</Text>
+                            <Text variant="link" component="a" href="http://ssbm-combo-viewer.tech/" target="_blank" rel="noopener noreferrer">Demo</Text>
+                            <br />
+                            <Text variant="link" component="a" href="https://github.com/WGoodsonDev/ssbm-combo-visualizer" target="_blank" rel="noopener noreferrer">Source</Text>
                         </td>
                     </tr>
                     <tr>
-                        <td>SSU Scheduler</td>
+                        <td>
+                        <Text
+                                component="span"
+                                align="center"
+                                variant="gradient"
+                                gradient={{ from: 'indigo', to: 'cyan', deg: 60 }}
+                                size="xl"
+                                weight={700}
+                                style={{ fontFamily: 'Greycliff CF, sans-serif' }}
+                            >
+                                SSU Scheduler
+                            </Text>
+                        </td>
                         <td>Full-stack application for scheduling classes in higher education. Senior Capstone Project at Sonoma State University.</td>
                         <td>React | D3 | MySQL | Koa</td>
                         <td>Proprietary</td>
                     </tr>
                     <tr>
-                        <td>Octane</td>
+                        <td>
+                        <Text
+                                component="span"
+                                align="center"
+                                variant="gradient"
+                                gradient={{ from: 'indigo', to: 'cyan', deg: 60 }}
+                                size="xl"
+                                weight={700}
+                                style={{ fontFamily: 'Greycliff CF, sans-serif' }}
+                            >
+                                Octane
+                            </Text>
+                        </td>
                         <td>Top-down, card-based, bullet hell shoot-em-up game with challenging boss fights. Awarded Winner: Best Gameplay at Sonoma State Tech Showcase by industry professionals from Visual Concepts, developers of the NBA 2K series.</td>
                         <td>Unreal Engine 4 | C++ | Blueprint</td>
                         <td>
-                            <Text variant="link" component="a" href="https://github.com/ogre9k/CS330_Final">Source</Text>
+                            <Text variant="link" component="a" href="https://github.com/ogre9k/CS330_Final" target="_blank" rel="noopener noreferrer">Source</Text>
                         </td>
                     </tr>
                     <tr>
-                        <td>Profile Site</td>
+                        <td>
+                        <Text
+                                component="span"
+                                align="center"
+                                variant="gradient"
+                                gradient={{ from: 'indigo', to: 'cyan', deg: 60 }}
+                                size="xl"
+                                weight={700}
+                                style={{ fontFamily: 'Greycliff CF, sans-serif' }}
+                            >
+                                Profile Site
+                            </Text>
+                        </td>
                         <td>Profile website for showing off my skills.</td>
                         <td>React | TypeScript | Mantine UI</td>
                         <td>
-                            <Text variant="link" component="a" href="http://warrengoodson.tech/">Demo</Text>
-                            <br/>
-                            <Text variant="link" component="a" href="https://github.com/WGoodsonDev/typescript-portfolio-mantine">Source</Text>
+                            <Text variant="link" component="a" href="http://warrengoodson.tech/" target="_blank" rel="noopener noreferrer">Demo</Text>
+                            <br />
+                            <Text variant="link" component="a" href="https://github.com/WGoodsonDev/typescript-portfolio-mantine" target="_blank" rel="noopener noreferrer">Source</Text>
                         </td>
                     </tr>
                 </tbody>
